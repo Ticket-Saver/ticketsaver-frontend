76662b51aca542d52c3e13f6655f3f60e0f334fb
Danyiel Colin <danyiel5978@gmail.com>
2024-06-26 16:46:52 -0600

    new file:   .prettierignore 	modified:   package.json 	modified:   src/pages/MyProfile.tsx

--- src/pages/MyProfile.tsx
+++ src/pages/MyProfile.tsx
@@ -1,40 +1,17 @@
-import { useEffect, useState } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 
 export default function MyProfile() {
-  const { user, logout, getAccessTokenSilently } = useAuth0()
-  const [token, setToken] = useState('')
-
-  const handleLogout = () => {
-    logout()
-  }
-
-  useEffect(() => {
-    const getToken = async () => {
-      try {
-        const accessToken = await getAccessTokenSilently()
-        setToken(accessToken)
-        console.log(accessToken) // For debugging purposes, remove in production
-      } catch (e) {
-        console.error(e)
-      }
-    }
-
-    getToken()
-  }, [getAccessTokenSilently])
-
+  const { user } = useAuth0()
   return (
-    <div className='container mx-auto px-4 py-8 bg-gray-100 rounded-lg shadow-md'>
-      <h1 className='text-2xl font-bold text-center mb-4'>My Profile</h1>
-      <div className='flex flex-col gap-4'>
-        <p className='text-gray-700'>Welcome, {user?.name || 'Guest'}!</p>
-        <p className='text-gray-500'>Email: {user?.email || 'Not provided'}</p>
-        <p className='text-gray-500'>Phone: {user?.phone_number || 'Not provided'}</p>
-        <p className='text-gray-500'>sub id: {user?.sub || 'Guest'}</p>
-        {/* Display the token for demonstration; remove or secure this in production */}
-        <p className='text-gray-500'>
-          Your access token: <code className='bg-gray-200 px-2 rounded-md'>{token}</code>
-        </p>
+    <div className='bg-blue-gray-800 text-white px-8 py-12 rounded-lg shadow-md'>
+      <h1 className='text-2xl font-bold mb-4'>My Profile</h1>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-2 mb-4'>
+        <p className='text-sm font-medium'>Email:</p>
+        <p className='text-gray-300'>{user?.email || 'Not provided'}</p>
+        <p className='text-sm font-medium'>Phone:</p>
+        <p className='text-gray-300'>{user?.phone_number || 'Not provided'}</p>
+        <p className='text-sm font-medium'>sub id:</p>
+        <p className='text-gray-300'>{user?.sub || 'Guest'}</p>
       </div>
     </div>
   )
