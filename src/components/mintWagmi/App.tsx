8f68120208a4f5bcf85d5ed51559539b811f37d5
Alejandro Ruiz Escamilla <alejandrore02@gmail.com>
2024-06-26 17:08:57 -0600

    added Wagmi Components

---
+++ src/components/mintWagmi/App.tsx
@@ -0,0 +1,19 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider } from 'wagmi'
+import { config } from './wagmi'
+import { WalletOptions } from './wallet-options'
+import { MintNFT } from './mint-nft'
+import { BlueCreateWalletButton } from './SmartWalletButton/walletButton'
+const queryClient = new QueryClient()
+
+export function App() {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <BlueCreateWalletButton/>
+        <WalletOptions />
+        <MintNFT />
+      </QueryClientProvider>
+    </WagmiProvider>
+  )
+}
