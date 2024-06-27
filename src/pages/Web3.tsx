d19a85fd0408c686cd6fe1528a34b73e47ad0ab9
Alejandro Ruiz Escamilla <alejandrore02@gmail.com>
2024-06-26 20:01:01 -0600

    removed wallet button

--- src/pages/Web3.tsx
+++ src/pages/Web3.tsx
@@ -3,15 +3,15 @@ import { WagmiProvider } from 'wagmi'
 import { config } from '../components/mintWagmi/wagmi'
 import { WalletOptions } from '../components/mintWagmi/wallet-options'
 import { MintNFT } from '../components/mintWagmi/mint-nft'
-import { BlueCreateWalletButton } from '../components/mintWagmi/SmartWalletButton/walletButton'
+//import { BlueCreateWalletButton } from '../components/mintWagmi/SmartWalletButton/walletButton'
 
 const queryClient = new QueryClient()
 
-export default function Wallet() {
+export default function Web3() {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <BlueCreateWalletButton/>
+      
         <WalletOptions />
         <MintNFT />
       </QueryClientProvider>
