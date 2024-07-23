7ad5ae57667da48f4bcecc6317355197237e2978
alejandrore02 <alejandrore02@gmail.com>
2024-07-23 10:46:39 -0600

    logs

--- netlify/utils/supabase.js
+++ netlify/utils/supabase.js
@@ -1,12 +1,13 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey)
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+
+const supabase = await createClient(supabaseUrl, supabaseAnonKey);
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Missing Supabase URL or Anon Key in environment variables')
-}
+    throw new Error('Missing Supabase URL or Anon Key in environment variables')
+  }
 
 export default supabase
