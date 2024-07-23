19e7277b838826b557d6c666941f1d17dbbacb19
alejandrore02 <alejandrore02@gmail.com>
2024-07-23 13:03:08 -0600

    changed supabaseClient

--- netlify/functions/fetchTakenSeats.js
+++ netlify/functions/fetchTakenSeats.js
@@ -1,4 +1,16 @@
-import {supabase} from '../utils/supabaseClientClient'
+//import {supabase} from '../utils/supabaseClient'
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey)
+console.log('Supabase client created:', supabase)
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Missing Supabase URL or Anon Key in environment variables')
+}
+console.log('Supabase client created:', supabase)
 
 export const handler = async (event, _context) => {
   if (event.httpMethod !== 'POST') {
--- netlify/functions/lockSeat.js
+++ netlify/functions/lockSeat.js
@@ -1,4 +1,15 @@
-import {supabase} from '../utils/supabaseClient'
+//import {supabase} from '../utils/supabaseClient'
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey)
+console.log('Supabase client created:', supabase)
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Missing Supabase URL or Anon Key in environment variables')
+}
 /*
 interface SeatUpdateRequest {
   Seat: string;
