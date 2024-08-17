72b28a482b6de49efe787a3964ebf4c3559bea89
carlosalaniz <carlosglvn93@gmail.com>
2024-08-17 00:28:33 -0500

    bcrypt password checking

--- netlify/functions/checkSeatState.ts
+++ netlify/functions/checkSeatState.ts
@@ -1,5 +1,6 @@
 import { Handler, HandlerEvent } from "@netlify/functions";
 import { supabase } from '../utils/supabaseClient'
+import bcrypt from 'bcryptjs'
 interface DataDict {
   client_info: string
   ticket_tag: string
@@ -10,7 +11,8 @@ interface DataDict {
 }
 
 const scanPassword = process.env.SCAN_PASSWORD;
-export const handler: Handler = async (event, context) => {
+
+export const handler: Handler = async (event) => {
   if (event.httpMethod !== 'POST' || !event.body) {
     return {
       statusCode: 405,
@@ -20,7 +22,8 @@ export const handler: Handler = async (event, context) => {
 
   const data: DataDict = JSON.parse(event.body)
 
-  if (data.password !== scanPassword) {
+  //check if password is correct
+  if (bcrypt.compareSync("B4c0/\/", scanPassword)) {
     return {
       statusCode: 401,
       body: JSON.stringify({ error: 'Unauthorized' })
@@ -28,7 +31,7 @@ export const handler: Handler = async (event, context) => {
   }
 
   //check in supabase if ticket exists user ticket_token
-  const { data: ticket, error } = await supabase
+  const { data: ticket } = await supabase
     .from('tickets')
     .select('*')
     .eq('ticket_token', data.ticket_token)
@@ -36,7 +39,7 @@ export const handler: Handler = async (event, context) => {
 
   //if ticket does not exist, insert ticket_token and the scan date into tickets table
   if (ticket === null) {
-    const { data: newTicket, error } = await supabase
+    await supabase
       .from('tickets')
       .insert([
         { ticket_token: data.ticket_token, scan_date: new Date() }
