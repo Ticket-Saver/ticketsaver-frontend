c63ff96cca8d626807132d4cf8fec79c86ca22a0
EpsyUwU <estebantareas2003@gmail.com>
2024-08-21 15:20:17 -0600

    integration social medias link, and remove input suscription

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -36,24 +36,6 @@ export default function Footer() {
               FAQS
             </a>
           </div>
-          <div className='w-full col-span-full md:col-auto'>
-            <span className='footer-title'>Newsletter</span>
-            <div className='form-control w-full'>
-              <label className='label'>
-                <span className='label-text'>Enter your email address</span>
-              </label>
-              <div className='relative'>
-                <input
-                  type='text'
-                  placeholder='user@site.com'
-                  className='input input-bordered w-full pr-16'
-                />
-                <button className='btn btn-primary absolute top-0 right-0 rounded-l-none'>
-                  Subscribe
-                </button>
-              </div>
-            </div>
-          </div>
         </div>
 
         <div className='flex flex-col sm:flex-row items-center border-t border-base-300 py-4 gap-2'>
@@ -61,20 +43,14 @@ export default function Footer() {
             <p>© 2022 Company, Inc. All rights reserved.</p>
           </div>
           <div className='grid grid-flow-col gap-4'>
-            <a className='link link-secondary' href='#!'>
+            <a className='link link-secondary' href='https://www.facebook.com/ticketsaver1?mibextid=LQQJ4d'>
               <i className='bi bi-facebook text-xl'></i>
             </a>
-            <a className='link link-secondary' href='#!'>
+            <a className='link link-secondary' href='https://www.instagram.com/ticket.saver?igsh=MWdtdXF0b2VudGk5'>
               <i className='bi bi-instagram text-xl'></i>
             </a>
-            <a className='link link-secondary' href='#!'>
+            <a className='link link-secondary' href='https://x.com/ticket_saver?s=08'>
               <i className='bi bi-twitter text-xl'></i>
-            </a>
-            <a className='link link-secondary' href='#!'>
-              <i className='bi bi-github text-xl'></i>
-            </a>
-            <a className='link link-secondary' href='#!'>
-              <i className='bi bi-dribbble text-xl'></i>
             </a>
           </div>
         </div>
