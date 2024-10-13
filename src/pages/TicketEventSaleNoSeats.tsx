84555703095d566513db9e12e84fd8e992d0c209
Alejandro Ruiz <alejandrore02@gmail.com>
2024-10-13 12:57:27 -0600

    improved UI no seats events

--- src/pages/TicketEventSaleNoSeats.tsx
+++ src/pages/TicketEventSaleNoSeats.tsx
@@ -34,8 +34,10 @@ export default function TicketSelectionNoSeat() {
   const [zoneData, setZoneData] = useState<any>({})
   const [venueInfo, setVenue] = useState<any>(null)
   const [imageUrl, setImageUrl] = useState<string | null>(null)
+  const [ticketQuantities, setTicketQuantities] = useState<{ [key: string]: number }>({})
   const navigate = useNavigate()
 
+  // Cargar la imagen del evento
   useEffect(() => {
     const loadImage = async () => {
       try {
@@ -49,6 +51,7 @@ export default function TicketSelectionNoSeat() {
     loadImage()
   }, [label])
 
+  // Cargar los datos de las zonas y los precios
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -110,31 +113,62 @@ export default function TicketSelectionNoSeat() {
     phone: user?.phone_number
   }
 
+  // Calcular el total de boletos en el carrito
+  const totalTicketsInCart = cart.length
+
+  // Calcular el costo total
   const totalCost = cart.reduce((acc, curr) => acc + curr.price_final, 0)
 
-  const handleBuyTicket = (zoneLabel: string, priceType: string) => {
-    const issuedAt = Date.now()
-    const cartLength = cart.length
-    const price_base = priceTagList[priceType]?.price_base / 100
-    if (!price_base) return
+  // Manejar cambios en la cantidad de boletos
+  const handleTicketQuantityChange = (
+    zoneLabel: string,
+    priceType: string,
+    newQuantity: number
+  ) => {
+    // Limitar el número total de boletos en el carrito a 10
+    const currentTotalTickets = totalTicketsInCart
+    const currentQuantity = cart.filter(
+      (ticket) => ticket.zoneName === zoneLabel && ticket.priceType === priceType
+    ).length
 
-    const newTicketId = ticketId(label || '', zoneLabel, cartLength + 1, issuedAt)
+    const totalNewTickets = currentTotalTickets - currentQuantity + newQuantity
 
-    setCart((prev: Cart[] | undefined) => [
-      ...(prev || []),
-      {
-        price_base: price_base,
-        price_final: priceTagList[priceType].price_final / 100,
-        zoneName: zoneLabel,
-        seatLabel: newTicketId,
-        seatType: priceType,
-        subZone: zoneLabel,
-        priceType: priceType,
-        ticketId: newTicketId,
-        issuedAt: issuedAt,
-        numberOfTicket: cartLength + 1
+    if (totalNewTickets <= 10) {
+      setTicketQuantities((prev) => ({
+        ...prev,
+        [`${zoneLabel}-${priceType}`]: newQuantity
+      }))
+
+      if (newQuantity > currentQuantity) {
+        const issuedAt = Date.now()
+        const newTickets: Cart[] = Array.from(
+          { length: newQuantity - currentQuantity },
+          (_, index) => {
+            const newTicketId = ticketId(label || '', zoneLabel, cart.length + index + 1, issuedAt)
+            return {
+              price_base: priceTagList[priceType]?.price_base / 100,
+              price_final: priceTagList[priceType]?.price_final / 100,
+              zoneName: zoneLabel,
+              seatLabel: newTicketId,
+              seatType: priceType,
+              subZone: zoneLabel,
+              priceType: priceType,
+              ticketId: newTicketId
+            }
+          }
+        )
+
+        setCart((prev) => [...prev, ...newTickets])
+      } else if (newQuantity < currentQuantity) {
+        const ticketsToRemove = cart
+          .filter((ticket) => ticket.zoneName === zoneLabel && ticket.priceType === priceType)
+          .slice(0, currentQuantity - newQuantity)
+
+        setCart((prev) => prev.filter((ticket) => !ticketsToRemove.includes(ticket)))
       }
-    ])
+    } else {
+      alert('You cannot add more than 10 tickets in total.')
+    }
   }
 
   const handleCheckout = () => {
@@ -156,22 +190,11 @@ export default function TicketSelectionNoSeat() {
     navigate('/checkout')
   }
 
-  const handleRemoveTicket = (ticketId: string) => {
-    // Elimina solo el ticket específico basado en su `ticketId`
-    const updatedCart = cart.filter((ticket) => ticket.ticketId !== ticketId)
-
-    // Actualiza el carrito
-    setCart(updatedCart)
-  }
-
   return (
-    <div className='bg-white'>
-      <div className='relative bg-gray-100'>
-        {/* Event Header */}
+    <div className='flex flex-col min-h-screen bg-white'>
+      <div className='relative flex-grow bg-gray-100'>
         <div className='absolute inset-0'>
-          {/* Cover Image */}
           <div className='relative h-96'>
-            {/* Event Profile Image */}
             {imageUrl && (
               <img
                 src={imageUrl}
@@ -179,21 +202,23 @@ export default function TicketSelectionNoSeat() {
                 className='absolute inset-0 w-full h-full object-cover'
               />
             )}
-            {/* Overlay for blur effect */}
             <div className='absolute inset-0 bg-black opacity-50'></div>
           </div>
         </div>
-        <div className='relative z-10 p-8'>
-          {/* Sección de la tabla de precios */}
+
+        <div className='relative z-10 flex flex-col items-center justify-start p-8'>
+          {/* Sección de precios y selección de boletos */}
           {zoneData.zones && Object.keys(zoneData.zones).length > 0 ? (
-            <div className='bg-white rounded-lg shadow-lg p-8 mb-8 border border-gray-300'>
+            <div className='w-full max-w-4xl bg-white rounded-lg shadow-lg p-8 mb-8 border border-gray-300'>
               <h2 className='text-2xl font-bold mb-6 text-black'>Ticket Prices</h2>
               <table className='w-full'>
                 <thead>
                   <tr>
                     <th className='text-left text-black border-b-2 border-gray-300 pb-2'>Zone</th>
                     <th className='text-right text-black border-b-2 border-gray-300 pb-2'>Price</th>
-                    <th className='text-right text-black border-b-2 border-gray-300 pb-2'>Add</th>
+                    <th className='text-right text-black border-b-2 border-gray-300 pb-2'>
+                      Quantity
+                    </th>
                   </tr>
                 </thead>
                 <tbody>
@@ -210,12 +235,27 @@ export default function TicketSelectionNoSeat() {
                             <p className='font-bold'>${priceFinal?.toFixed(2)}</p>
                           </td>
                           <td className='text-right py-2 border-b border-gray-300'>
-                            <button
-                              className='bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 transition duration-300 mr-2'
-                              onClick={() => handleBuyTicket(zoneLabel, priceType)}
-                            >
-                              Add to Cart
-                            </button>
+                            <input
+                              type='number'
+                              min='0'
+                              max={
+                                10 -
+                                totalTicketsInCart +
+                                cart.filter(
+                                  (ticket) =>
+                                    ticket.zoneName === zoneLabel && ticket.priceType === priceType
+                                ).length
+                              }
+                              value={ticketQuantities[`${zoneLabel}-${priceType}`] || 0}
+                              onChange={(e) => {
+                                const value = Math.min(
+                                  10,
+                                  Math.max(0, parseInt(e.target.value, 10))
+                                )
+                                handleTicketQuantityChange(zoneLabel, priceType, value)
+                              }}
+                              className='w-20 text-right border rounded-md p-3 text-lg'
+                            />
                           </td>
                         </tr>
                       )
@@ -229,25 +269,17 @@ export default function TicketSelectionNoSeat() {
           )}
 
           {/* Sección del carrito */}
-          {cart.length > 0 && (
-            <div className='bg-white rounded-lg shadow-lg p-8 border border-gray-300'>
+          {cart.length > 0 ? (
+            <div className='w-full max-w-4xl bg-white rounded-lg shadow-lg p-8 border border-gray-300'>
               <h2 className='text-2xl font-bold mb-6 text-black'>Cart</h2>
               {cart.map((ticket) => (
                 <div
-                  key={ticket.ticketId} // Usa ticketId como key
-                  className='mb-4 pb-4 border-b-2 border-gray-300 flex justify-between text-black'
+                  key={ticket.ticketId}
+                  className={`mb-4 pb-4 border-b-2 border-gray-300 flex justify-between text-black`}
                 >
                   <div>
                     <span className='font-semibold'>Ticket - {ticket.zoneName}</span>
-                    <p> Price: ${ticket.price_final.toFixed(2)}</p>
-                  </div>
-                  <div className='flex items-center'>
-                    <button
-                      className='bg-red-600 text-white py-2 px-4 rounded-md hover:bg-red-700 transition duration-300'
-                      onClick={() => handleRemoveTicket(ticket.ticketId)}
-                    >
-                      Remove
-                    </button>
+                    <p>Price: ${ticket.price_final.toFixed(2)}</p>
                   </div>
                 </div>
               ))}
@@ -260,6 +292,7 @@ export default function TicketSelectionNoSeat() {
                   <p className='text-xl font-bold text-black'>${totalCost.toFixed(2)}</p>
                 </div>
               </div>
+
               <div className='flex justify-center mt-8'>
                 <Link to='/checkout'>
                   <button
@@ -270,6 +303,10 @@ export default function TicketSelectionNoSeat() {
                   </button>
                 </Link>
               </div>
+            </div>
+          ) : (
+            <div className='w-full flex-grow flex flex-col justify-center items-center'>
+              <p className='text-2xl font-bold text-gray-500'>Your cart is empty</p>
             </div>
           )}
         </div>
