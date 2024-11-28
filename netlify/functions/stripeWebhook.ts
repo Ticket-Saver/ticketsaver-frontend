490b83d14aef54b12edd8053b39de1ecd41ea6d4
Alejandro Ruiz <alejandrore02@gmail.com>
2024-11-28 07:59:56 -0600

    fixed webhook

--- netlify/functions/stripeWebhook.ts
+++ netlify/functions/stripeWebhook.ts
@@ -34,7 +34,7 @@ export const handler: Handler = async (event, _context) => {
       const purchaseDate = new Date(eventObject.created * 1000).toISOString() // Convierte la fecha a ISO formato
       console.log(`event label: ${event_label}`)
 
-      let totalSeatsSold = 0 // Para sumar los asientos vendidos en este pago
+      let totalSeatsSoldBySubZone: { [key: string]: number } = {} // Para llevar el total vendido por cada subZone
 
       for (const item of items) {
         const description = item.description
@@ -68,42 +68,53 @@ export const handler: Handler = async (event, _context) => {
           }
         }
         const quantity = item.quantity || 1 // Suponiendo que `quantity` está en el objeto del ítem
-        totalSeatsSold += quantity
+        totalSeatsSoldBySubZone[subZone] = (totalSeatsSoldBySubZone[subZone] || 0) + quantity
       }
 
-      const { data: currentData, error } = await supabase
-        .from('eventseatstatus')
-        .select('sold_seats')
-        .eq('event_id', event_label)
-        .single()
-      if (error) {
-        console.error('Error al obtener sold_seats:', error)
-        return {
-          statusCode: 500,
-          body: JSON.stringify({ error: 'Error fetching sold_seats' })
+      for (const [subZone, seatsSold] of Object.entries(totalSeatsSoldBySubZone)) {
+        const { data: currentData, error } = await supabase
+          .from('eventseatstatus')
+          .select('sold_seats')
+          .eq('event_id', event_label)
+          .eq('seat_type', subZone)
+          .single()
+
+        if (error) {
+          console.error(`Error al obtener sold_seats para subZone ${subZone}:`, error)
+          return {
+            statusCode: 500,
+            body: JSON.stringify({ error: `Error fetching sold_seats for subZone ${subZone}` })
+          }
         }
-      }
-      const newSoldSeats = (currentData?.sold_seats || 0) + totalSeatsSold
-      const { data: statusData, error: statusError } = await supabase
-        .from('EventSeatStatus')
-        .update({
-          sold_seats: newSoldSeats,
-          updated_at: new Date().toISOString()
-        })
-        .eq('event_id', event_label)
-      console.log(statusData)
-      if (statusError) {
-        console.error('Error en la actualización de EventSeatStatus:', statusError)
-        return {
-          statusCode: 500,
-          body: JSON.stringify({ error: 'Error en la actualización de EventSeatStatus' })
+
+        const newSoldSeats = (currentData?.sold_seats || 0) + seatsSold
+
+        const { error: statusError } = await supabase
+          .from('eventseatstatus')
+          .update({
+            sold_seats: newSoldSeats,
+            updated_at: new Date().toISOString()
+          })
+          .eq('event_id', event_label)
+          .eq('seat_type', subZone)
+
+        if (statusError) {
+          console.error(
+            `Error en la actualización de EventSeatStatus para subZone ${subZone}:`,
+            statusError
+          )
+          return {
+            statusCode: 500,
+            body: JSON.stringify({
+              error: `Error en la actualización de EventSeatStatus para subZone ${subZone}`
+            })
+          }
         }
+
+        console.log(
+          `EventSeatStatus actualizado: ${seatsSold} asientos vendidos para el evento ${event_label}, subZone ${subZone}`
+        )
       }
-
-      console.log(
-        `EventSeatStatus actualizado: ${totalSeatsSold} asientos vendidos para el evento ${event_label}`
-      )
-
       return { statusCode: 200, body: JSON.stringify({ message: 'Webhook handled successfully' }) }
     } else {
       return { statusCode: 400, body: 'Event type not handled' }
