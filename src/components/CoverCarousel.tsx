767f7ef954c62d722a5c9aee13f7bbb91007bd81
Samuel Ambrosio <samuelambrosio99@gmail.com>
2026-03-06 19:32:31 -0600

    fix: cover

--- src/components/CoverCarousel.tsx
+++ src/components/CoverCarousel.tsx
@@ -3,51 +3,61 @@ import { useState, useEffect } from 'react'
 export default function CoverCarousel() {
   const [currentSlide, setCurrentSlide] = useState(0)
   const slides = [
+    // {
+    //   id: 1,
+    //   imageUrl: '/events/turbulence-sf.jpg',
+    //   caption: 'Welcome to Ticketsaver!'
+    // },
+    // {
+    //   id: 2,
+    //   imageUrl: '/events/IndiaYuridia.jpg',
+    //   caption: 'Discover amazing events!'
+    // },
+    // {
+    //   id: 3,
+    //   imageUrl: '/events/Leonas.jpg',
+    //   caption: 'We save you the seats so you can savour the night'
+    // },
+    // {
+    //   id: 4,
+    //   imageUrl: '/events/turbulence-sf.jpg',
+    //   caption: ''
+    // },
+    // {
+    //   id: 5,
+    //   imageUrl: '/events/turbulence-sj.jpg',
+    //   caption: ''
+    // }
     {
       id: 1,
-      imageUrl: '/events/turbulence-sf.jpg',
-      caption: 'Welcome to Ticketsaver!'
-    },
-    {
-      id: 2,
-      imageUrl: '/events/IndiaYuridia.jpg',
-      caption: 'Discover amazing events!'
-    },
-    {
-      id: 3,
-      imageUrl: '/events/Leonas.jpg',
-      caption: 'We save you the seats so you can savour the night'
-    },
-    {
-      id: 4,
-      imageUrl: '/events/turbulence-sf.jpg',
+      imageUrl: '/events/alucines-1.jpg',
       caption: ''
     },
     {
-      id: 5,
-      imageUrl: '/events/turbulence-sj.jpg',
+      id: 2,
+      imageUrl: '/events/alucines-2.jpg',
       caption: ''
     }
   ]
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setCurrentSlide(prevSlide => (prevSlide === slides.length - 1 ? 0 : prevSlide + 1))
+      setCurrentSlide((prevSlide) => (prevSlide === slides.length - 1 ? 0 : prevSlide + 1))
     }, 6000) // 6 seconds interval
 
     return () => clearInterval(intervalId)
   }, [slides.length])
 
   const nextSlide = () => {
-    setCurrentSlide(prevSlide => (prevSlide === slides.length - 1 ? 0 : prevSlide + 1))
+    setCurrentSlide((prevSlide) => (prevSlide === slides.length - 1 ? 0 : prevSlide + 1))
   }
 
   const prevSlide = () => {
-    setCurrentSlide(prevSlide => (prevSlide === 0 ? slides.length - 1 : prevSlide - 1))
+    setCurrentSlide((prevSlide) => (prevSlide === 0 ? slides.length - 1 : prevSlide - 1))
   }
 
   return (
-    <div className="carousel-container relative w-full">
+    <div className='carousel-container relative w-full'>
       {slides.map((slide, index) => (
         <div
           key={slide.id}
@@ -56,32 +66,32 @@ export default function CoverCarousel() {
           } flex justify-center items-center transition-opacity duration-500`}
           onClick={() => console.log('click a', slide.id)}
         >
-          <div className="w-full relative">
+          <div className='w-full relative'>
             <img
               src={slide.imageUrl}
               alt={`Slide ${slide.id}`}
-              className="w-full overflow-hidden object-cover"
+              className='w-full overflow-hidden object-cover'
               style={{
                 height: slide.id === 2 || slide.id === 3 ? '60vh' : '50vh',
                 width: slide.id === 2 || slide.id === 3 ? '100%' : '100%'
               }}
             />
-            <div className="absolute bottom-4 left-0 w-full flex justify-center">
-              <div className="bg-black bg-opacity-50 rounded-lg py-2 px-4">
-                <h1 className="text-white text-4xl font-bold">{slide.caption}</h1>
+            <div className='absolute bottom-4 left-0 w-full flex justify-center'>
+              <div className='bg-black bg-opacity-50 rounded-lg py-2 px-4'>
+                <h1 className='text-white text-4xl font-bold'>{slide.caption}</h1>
               </div>
             </div>
           </div>
         </div>
       ))}
       <button
-        className="absolute top-1/2 left-4 transform -translate-y-1/2 btn btn-circle btn-lg bg-neutral opacity-50"
+        className='absolute top-1/2 left-4 transform -translate-y-1/2 btn btn-circle btn-lg bg-neutral opacity-50'
         onClick={prevSlide}
       >
         ❮
       </button>
       <button
-        className="absolute top-1/2 right-4 transform -translate-y-1/2 btn btn-circle btn-lg bg-neutral opacity-50"
+        className='absolute top-1/2 right-4 transform -translate-y-1/2 btn btn-circle btn-lg bg-neutral opacity-50'
         onClick={nextSlide}
       >
         ❯
